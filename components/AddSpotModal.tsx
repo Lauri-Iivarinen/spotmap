9364ce56d48a9eaa92f33958c2d34d23d8437a32
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-08-05 16:28:41 +0300

    add spots to db with draggable marker

---
+++ components/AddSpotModal.tsx
@@ -0,0 +1,86 @@
+import { Button, TextInput } from "@react-native-material/core";
+import React, {useState} from "react";
+import { Modal,View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { User } from "../util/types";
+
+interface ModalProps {
+    visible: boolean;
+    toggleModal: () => void;
+    spotAdded: () => void;
+    lon: number;
+    lat: number;
+    user: User;
+    token: string;
+}
+
+export default function AddSpotModal(props: ModalProps) {
+
+    const [spotName, setSpotName] = useState<string>('')
+    const [spotDescription, setSpotDescription] = useState<string>('')
+
+    const submitNewSpot = async () => {
+        const body = {
+            "name": spotName,
+            "image": "url",
+            "description": spotDescription,
+            "lon": props.lon,
+            "lat": props.lat, 
+            "user": {
+                "id": props.user.id
+            }
+        }
+        console.log('spot:')
+        console.log(JSON.stringify(body))
+
+        try {
+            const response = await fetch("https://spotmapback-4682c78c99fa.herokuapp.com/api/spots", {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${props.token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+            const result = await response.json()
+            console.log(await result)
+            if (await result.name === spotName) {
+                props.spotAdded()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    //close modal
+    const cancelSubmit = () => {
+        setSpotName('')
+        setSpotDescription('')
+        props.toggleModal()
+    }
+    
+    return (
+        <Modal
+            visible={props.visible}
+            animationType="slide"
+        >
+            <View style={{
+                marginTop: '35%',
+                alignSelf: 'center',
+                backgroundColor: 'white',
+                borderRadius: 20,
+                padding: 60,
+                alignItems: 'center',
+            }}
+            >
+                <Text style={{fontSize: 25, marginBottom: 10}}>Add new spot</Text>
+                <TextInput style={{ width: 200, borderWidth: 1, marginBottom: 5 }} label='Spot name' value={spotName} onChangeText={(e) => setSpotName(e)}></TextInput>
+                <TextInput style={{ width: 200, borderWidth: 1 }} label='Spot description' value={spotDescription} onChangeText={(e) => setSpotDescription(e)}></TextInput>
+                <View style={{flexDirection: 'row', marginTop: 10}}>
+                    <Button title='submit' onPress={submitNewSpot}></Button>
+                    <Button title='cancel' onPress={cancelSubmit}></Button>
+                </View>
+            </View>
+        </Modal>
+    )
+}
--- components/MapSreen.tsx
+++ components/MapSreen.tsx
@@ -3,8 +3,9 @@ import { Platform, Text, View, StyleSheet } from 'react-native';
 import MapView, { Circle, Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { User, LocationType, Spot } from "../util/types";
-import { Snackbar } from "@react-native-material/core";
+import { Button, Snackbar } from "@react-native-material/core";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AddSpotModal from './AddSpotModal';
 
 //Home screen for logged in users
 export default function MapScreen({route, navigation}: any) {
@@ -21,24 +22,30 @@ export default function MapScreen({route, navigation}: any) {
     });
     const [spots, setSpots] = useState<Spot[]>([])
     const [snackbarVisible, setSnackbarVisible] = useState(false)
+    const [newSpotMarker, setNewSpotMarker] = useState<boolean>(false)
+    const [newSpotLat, setNewSpotLat] = useState<number>(0)
+    const [newSpotLon, setNewSpotLon] = useState<number>(0)
+    const [modalVisible, setModalVisible] = useState<boolean>(false)
 
     const toggleSnackbar = () => {
         setSnackbarVisible(true)
         setTimeout(() => setSnackbarVisible(false), 4000)
     }
 
+    const resetMarkerPos = () => {
+        setNewSpotLat(location.coords.latitude)
+        setNewSpotLon(location.coords.longitude)
+    }
+
     const fetchSpots = async () => {
         try {
             const response = await fetch("https://spotmapback-4682c78c99fa.herokuapp.com/api/spots")
             const result = await response.json()
-            console.log('SPOTS:')
-            console.log(await result)
             setSpots(await result)
             setErrorMsg('')
         } catch (error) {
             console.log(error)
         }
-        
     }
 
     useEffect(() => {
@@ -57,10 +64,66 @@ export default function MapScreen({route, navigation}: any) {
                 latitudeDelta: 0.03,
                 longitudeDelta: 0.022
             })
+            resetMarkerPos()
             fetchSpots()
             toggleSnackbar()
         })();
-      }, []);
+    }, []);
+    
+    //First shows marker on map, if marker has been shown and confirmed toggle modal that finished adding sopot
+    //(name description for now)
+    const addSpot = () => {
+        if (!newSpotMarker) setNewSpotMarker(true)
+        else {
+            setNewSpotMarker(false)
+            toggleModal()
+        }
+    }
+
+    //Update marker coordinates after dragging
+    const dragFinished = (e: any) => {
+        console.log(e.nativeEvent.coordinate)
+        const coords = e.nativeEvent.coordinate
+        setNewSpotLat(coords.latitude)
+        setNewSpotLon(coords.longitude)
+    }
+
+    const toggleModal = () => {
+        if (modalVisible) resetMarkerPos()
+        setModalVisible(!modalVisible)
+    }
+
+    //If POST was succesful inside modal
+    const spotAdded = () => {
+        toggleModal()
+        fetchSpots()
+        resetMarkerPos()
+    }
+
+    const cancelAddSpot = () => {
+        resetMarkerPos()
+        setNewSpotMarker(false)
+    }
+
+    //Prevents overlap of snackbar and buttons because requires 2 different conditionals
+    const buttons = () => !newSpotMarker
+        ? <Button
+            title='Add spot'
+            style={{ width: '100%', height: 50, marginTop: 50, justifyContent: 'center', alignItems: 'center' }}
+            onPress={addSpot}
+        />
+        : <View style={{flexDirection: 'row'}}>
+            <Button
+                title='Confirm'
+                style={{ width: '50%', height: 50, marginTop: 50, justifyContent: 'center', alignItems: 'center' }}
+                onPress={addSpot}
+            />
+            <Button
+                title='Cancel'
+                style={{ width: '50%', height: 50, marginTop: 50, justifyContent: 'center', alignItems: 'center' }}
+                onPress={cancelAddSpot}
+            />
+        </View>
 
     if (errorMsg.length > 0) {
         return (<View><Text>{errorMsg}</Text></View>)
@@ -70,18 +133,31 @@ export default function MapScreen({route, navigation}: any) {
                 {snackbarVisible &&
                     <Snackbar
                         message={'Loged in as ' + user.username}
-                        style={{width: '100%', marginTop: 50}}
+                        style={{width: '100%', height: 50, marginTop: 50}}
                     />
+                }
+                {!snackbarVisible &&
+                    buttons()
                 }
                 <MapView
                     style={{ width: '100%', height: '100%' }}
                     initialRegion={region}
                     showsUserLocation={true}
                 >
+                {newSpotMarker &&
+                <Marker
+                    coordinate={{ latitude: newSpotLat, longitude: newSpotLon }}
+                    pinColor='blue'
+                    isPreselected={true}
+                    draggable
+                    onDragEnd={(e) => dragFinished(e)}
+                />
+                }
                 {spots.map((spot, index) => {
                     return (<Marker key={index} title={spot.name} description={spot.description} coordinate={{latitude: spot.lat, longitude: spot.lon}}></Marker>)
                 })}
                 </MapView>
+                <AddSpotModal visible={modalVisible} toggleModal={toggleModal} spotAdded={spotAdded} lon={newSpotLon} lat={newSpotLat} user={user} token={token}></AddSpotModal>
             </View>
         )
     }
