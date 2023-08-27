024503f3b7d9b48dd4cc0bc54537ba41c65e7b73
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-08-27 14:29:45 +0300

    password validator and tests

--- tests/TextValidation.test.js
+++ tests/TextValidation.test.js
@@ -1,4 +1,4 @@
-import { firstLetterToUpper, validateUsername } from "../util/TextValidation"
+import { firstLetterToUpper, validatePassword, validateUsername } from "../util/TextValidation"
 
 test('Validate username, too short', () => {
     expect(validateUsername('user')).toBe(false)
@@ -19,6 +19,34 @@ test('Validate username, OK', () => {
 
 test('First character is returned in uppercase', () => {
     expect(firstLetterToUpper('cat')).toBe('Cat')
-    expect(firstLetterToUpper('dog')).toBe('Dog')
+    expect(firstLetterToUpper('DOG')).toBe('DOG')
     expect(firstLetterToUpper('Username')).toBe('Username')
+})
+
+test('Validate password, too short', () => {
+    expect(validatePassword('passwrd')).toBe(false)
+})
+
+test('Validate password, too long', () => {
+    expect(validatePassword('passwordpasswordpasswordpasswordpasswordpasswor')).toBe(false)
+})
+
+test('Validate password, no capital letter', () => {
+    expect(validatePassword('passw0rd!')).toBe(false)
+})
+
+test('Validate password, no number', () => {
+    expect(validatePassword('Password!')).toBe(false)
+})
+
+test('Validate password, no special char', () => {
+    expect(validatePassword('Passw0rd')).toBe(false)
+})
+
+test('Validate password, no small letter', () => {
+    expect(validatePassword('PASSW0RD!')).toBe(false)
+})
+
+test('Validate password, ok', () => {
+    expect(validatePassword('Passw0rd!')).toBe(true)
 })
--- util/TextValidation.tsx
+++ util/TextValidation.tsx
@@ -1,8 +1,5 @@
 export const validateUsername = (text: string) => {
-    if (text.trim().length < 5) {
-        return false
-    }
-    if (text.trim().length > 15) {
+    if (text.trim().length < 5 || text.trim().length > 15) {
         return false
     }
     const chars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
@@ -17,4 +14,18 @@ export const firstLetterToUpper = (string: string) => {
     const stringEnd = string.substr(1, (string.length - 1))
     
     return firstChar.toUpperCase() + stringEnd
+}
+
+export const validatePassword = (string: string) => {
+    if (string.trim().length < 7 || string.trim().length > 30) {
+        return false
+    }
+    const chars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const small = /[a-z]/
+    const large = /[A-Z]/
+    const numbers = /[0-9]/
+
+    if (chars.test(string) && small.test(string) && large.test(string) && numbers.test(string)) return true
+
+    return false
 }
